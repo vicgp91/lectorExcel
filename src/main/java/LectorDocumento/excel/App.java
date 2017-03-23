72ef246d91e7e9208593ea586af0fc0a8df92ed0
vicgp91 <vicgp_24@hotmail.com>
2017-03-23 16:12:44 -0500

    cambios

--- src/main/java/LectorDocumento/excel/App.java
+++ src/main/java/LectorDocumento/excel/App.java
@@ -1,25 +1,11 @@
 package LectorDocumento.excel;
-import java.io.File;
-import java.io.IOException;
-
-import javax.imageio.ImageIO;
-
 import presentacion.Formulario;
-import utils.ManejadorImagenes;
+
 
 
 public class App {
 	@SuppressWarnings("unused")
 	public static void main(String[] args) {
-		  //  Formulario form = new Formulario();   
-		ManejadorImagenes manejador = new ManejadorImagenes();
-		File file =new File("C:\\Users\\LogicStudio\\Documents\\inmobilizacioninfo2.png");
-		
-		  try {
-			ImageIO.write(manejador.crearPNG(), "png", file);
-		} catch (IOException e) {
-			
-			e.printStackTrace();
-		}
+		 Formulario form = new Formulario();   
 	}
 }
